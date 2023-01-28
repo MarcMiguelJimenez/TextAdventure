eb22979bba125e22da6c662839f66b542c78f8c6
Marc Miguel <marcmiguel@Marcs-Air.fios-router.home>
2023-01-27 21:20:48 -0500

    change

---
+++ Arrow.java
@@ -0,0 +1,5 @@
+public class Arrow extends Weapon{
+    public Arrow(){
+        super("Arrow", "A pointy Arrow. Somewhat more dangerous than a sword.", 10, 35);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+      Gold goldBar = new Gold(10);
+      Weapon weapon1 = new Weapon("Super weapon", "Super 2 weapon", 10, 50);
+      Sword magicSword = new Sword();
+      Pillow softPillow = new Pillow();
+      Mace bigMace = new Mace();
+      Arrow sharpArrow = new Arrow();
+    }
+}
